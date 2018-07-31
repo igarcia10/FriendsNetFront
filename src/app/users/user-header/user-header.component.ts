d5a340c3fee8de4d37b2a99e0137ea182d1ae847
igarcia10 <40008812+igarcia10@users.noreply.github.com>
2018-07-31 18:08:27 +0200

    added json-server and implemented users and posts

--- src/app/users/user-header/user-header.component.ts
+++ src/app/users/user-header/user-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/user.model';
 
 @Component({
   selector: 'app-user-header',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserHeaderComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  constructor() {
+   }
 
   ngOnInit() {
   }
