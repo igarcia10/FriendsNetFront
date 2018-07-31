67a4fe207eb6c72a1a0cc16ba6e07d04aa7d209b
ivangarcia <ivangarciatecnico@gmail.com>
2018-07-31 22:49:24 +0200

    implemented bio in user (profile) and passed user to post list

--- src/app/users/bio/bio.component.ts
+++ src/app/users/bio/bio.component.ts
@@ -13,6 +13,7 @@ export class BioComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.user.bio = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat a sunt magni minima dolorum. Magnam at adipisci dolorem alias doloribus autem deleniti fuga. Sit, tempore accusantium nulla rerum maxime aut.";
   }
 
 }
