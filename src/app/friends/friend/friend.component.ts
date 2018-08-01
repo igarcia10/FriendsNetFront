33eae84b921420eb3b4c8d7b82cd6b2c5c130895
ivangarcia <ivangarciatecnico@gmail.com>
2018-08-01 12:29:14 +0200

    implemented profile friend list

--- src/app/friends/friend/friend.component.ts
+++ src/app/friends/friend/friend.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../../users/shared/user.model';
 
 @Component({
   selector: 'app-friend',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FriendComponent implements OnInit {
 
+  @Input() user: User;
   deletable = false;
 
   constructor() { }
