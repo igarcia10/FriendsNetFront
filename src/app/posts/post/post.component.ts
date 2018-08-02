f92fd2e48f5556abce2f20ed4b33969afce28366
igarcia10 <40008812+igarcia10@users.noreply.github.com>
2018-08-02 17:24:40 +0200

    implemented user likes

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../shared/post.model';
 import { Like } from '../shared/like.model';
 import { User } from '../../users/shared/user.model';
+import { PostService } from '../shared/post.service';
 
 @Component({
   selector: 'app-post',
@@ -19,7 +20,7 @@ export class PostComponent implements OnInit {
   argLikes = 0;
   userArgLike = false;
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
     this.post.likes.forEach((like: Like) => {
@@ -50,6 +51,50 @@ export class PostComponent implements OnInit {
           }
       }
     });
+  }
+
+  like(likeType: number) {
+    const like = new Like();
+    like.user = this.user;
+    this.postService.addLike(this.post.id, this.user.id, likeType);
+    if (this.userCoolLike || this.userDontcareLike || this.userArgLike) {
+      if (this.userCoolLike) {
+        this.userCoolLike = false;
+        this.coolLikes--;
+      } else if (this.dontcareLikes) {
+        this.userDontcareLike = false;
+        this.dontcareLikes--;
+      } else {
+        this.userArgLike = false;
+        this.argLikes--;
+      }
+    }
+    switch (likeType) {
+      case 1:
+        {
+          if (like.user.id === this.user.id) {
+            this.userCoolLike = true;
+          }
+          this.coolLikes++;
+          break;
+        }
+      case 2:
+        {
+          if (like.user.id === this.user.id) {
+            this.userDontcareLike = true;
+          }
+          this.dontcareLikes++;
+          break;
+        }
+        case 3:
+        {
+          if (like.user.id === this.user.id) {
+            this.userArgLike = true;
+          }
+          this.argLikes++;
+          break;
+        }
+    }
   }
 
 }
